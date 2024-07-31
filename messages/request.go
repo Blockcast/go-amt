2700cc3fe133b15f59545df3b5d97478721397b2
clararfe <clarar@qualabs.com>
2024-07-31 17:42:54 -0300

    deleted comments and unused vars in request

--- messages/request.go
+++ messages/request.go
@@ -45,89 +45,14 @@ func (rm *RequestMessage) MarshalBinary() ([]byte, error) {
 
 	var myvar [4]byte
 
-	// Fill in the bit fields according to their sizes
 	myvar[0] = 0x03 // version + type
 	myvar[1] = 0x00 // rsvd1 + p_flag
 	myvar[2] = 0x00 // rsvd2 (high byte)
 	myvar[3] = 0x00 // rsvd2 (low byte)
 
-	// Append the nonce (4 bytes)
-	//nonce := []byte{0x12, 0x34, 0x56, 0x78} // Example nonce value
-	// Combine bit fields and nonce
 	result := append(myvar[:], rm.Nonce[:]...)
 
 	return result, nil
-	// var b byte
-	// b |= (0 << 4) // Shift Version to the first nibble
-	// b |= byte(3)  // Set Type in the next nibble
-	// ret := []byte{b, 0, 0, 0}
-
-	// ret = append(ret, 0) // P flag
-
-	// ret = append(ret, 0, 0)
-	// ret = append(ret, rm.Nonce[:]...)
-
-	// fmt.Println("request")
-	// // [3 0 0 0 1 0 0 98 34 51 37]
-	// // [3 0 0 0 0 0 0 111 70 66 170]
-	// fmt.Println(ret)
-
-	// return ret, nil
-
-	// relayRequest := &AMTRelayRequest{
-	// 	Version:   0,
-	// 	Type:      1,
-	// 	Reserved1: 0,
-	// 	PFlag:     1,
-	// 	Reserved2: 0,
-	// 	Nonce:     [4]byte{0x01, 0x02, 0x03, 0x04},
-	// }
-
-	// // Convert to bytes
-	// bytes := relayRequest.ToBytes()
-	// fmt.Printf("Byte representation: %x\n", bytes)
-
-	// return bytes, nil
-
-	// var b byte
-	// b |= (0 << 4) // Shift Version to the first nibble
-	// b |= byte(3)  // Set Type in the next nibble
-	// ret := []byte{b}
-
-	// // The second byte: Reserved bits + P flag
-	// b = 0
-	// b |= (1 << 3) // Set P flag to 1 at the correct position (bit 3 of the second byte)
-	// ret = append(ret, b)
-
-	// // Two more reserved bytes (Byte 2 and Byte 3)
-	// ret = append(ret, 0, 0)
-
-	// // Append the nonce (4 bytes)
-	// ret = append(ret, rm.Nonce[:]...)
-
-	// return ret, nil
-
-	// buf := new(bytes.Buffer)
-	// // if err := binary.Write(buf, binary.BigEndian, rm.Header.Version); err != nil {
-	// // 	return nil, err
-	// // }
-	// // if err := binary.Write(buf, binary.BigEndian, rm.Header.Type); err != nil {
-	// // 	return nil, err
-	// // }
-	// pFlagByte := uint8(0)
-	// // if rm.PFlag {
-	// // 	pFlagByte = 0x80 // Sets the first bit to 1
-	// // }
-	// if err := buf.WriteByte(pFlagByte); err != nil {
-	// 	return nil, err
-	// }
-	// if err := binary.Write(buf, binary.BigEndian, rm.Reserved); err != nil {
-	// 	return nil, err
-	// }
-	// if err := binary.Write(buf, binary.BigEndian, rm.Nonce); err != nil {
-	// 	return nil, err
-	// }
-	// return buf.Bytes(), nil
 }
 
 func DecodeRequestMessage(data []byte) (*RequestMessage, error) {
@@ -155,11 +80,6 @@ func DecodeRequestMessage(data []byte) (*RequestMessage, error) {
 	}
 	return rm, nil
 }
-
-const (
-	VersionLen = 4 // Length in bits
-	MsgTypeLen = 4 // Length in bits
-)
 
 type AMTRelayRequest struct {
 	Version   uint8   // 4 bits
