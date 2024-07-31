1f6ce462eea60be9e4af962e6d0636b683849dd8
clararfe <clarar@qualabs.com>
2024-07-31 17:41:26 -0300

    deleted comments

--- cmd/amt_gw/main.go
+++ cmd/amt_gw/main.go
@@ -7,20 +7,14 @@ import (
 )
 
 func main() {
-	// relay := "162.250.137.254"
-	// relay := "162.250.136.101"
-	// source := "83.97.94.146"
-	// group := "232.1.2.3"
-
 	relay := "162.250.137.254"
-	multicast := "232.162.250.140" // group
+	multicast := "232.162.250.140"
 	source := "162.250.138.201"
 
 	dataChannel := make(chan []byte)
 	amt.StartGateway(relay, source, multicast, dataChannel)
-
 	for data := range dataChannel {
+		fmt.Println("!!!!!")
 		fmt.Println("Received data:", data)
 	}
-
 }
